ac1a5d52784197cd57edbe8881d6a6e42b580efa
Erik Wickstrom <erik@erikwickstrom.com>
2010-08-05 12:31:44 -0700

    Added some exclusions to the log4j dependency.  Upgraded to swank-clojure 1.2.1

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject org.clojars.erikcw/sdb "1.0.0-SNAPSHOT"
   :description "A Clojure library for working with Amazon SimpleDB"
-  :dependencies [[log4j/log4j "1.2.15"]
+  :dependencies [[log4j/log4j "1.2.15" :exclusions [javax.mail/mail
+                                                   javax.jms/jms
+                                                   com.sun.jdmk/jmxtools
+                                                   com.sun.jmx/jmxri]]
                  [com.amazonaws/aws-java-sdk "1.0.002"]
                  [org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
+  :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-clojars "0.5.0-SNAPSHOT"]]
   :source-path "src")
